6ddfbec6beff17db4d3d579f7bae971b4736b7e9
Rimba Prayoga <rimba47prayoga@gmail.com>
2022-10-25 15:37:02 +0700

    fix: slack restrict char.

--- src/kri_lib/logger/notification.py
+++ src/kri_lib/logger/notification.py
@@ -38,68 +38,76 @@ def notify_to_slack(
         responsible = email
 
     slack_url = settings.LOGGING.get('SLACK').get('HOOKS_URL')
-    payload = {
-        "text": repr(exception),
-        "blocks": [
-            {
-                "type": "header",
-                "text": {
-                    "type": "plain_text",
-                    "text": repr(exception),
-                    "emoji": True
-                }
-            },
-            {
-                "type": "section",
-                "fields": [
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Service:*\n{service_name}"
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Branch:*\n{get_git_branch()}"
-                    }
-                ]
-            },
-            {
-                "type": "section",
-                "fields": [
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Endpoint:*\n`{endpoint}`"
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*API ID:*\n`{GLOBALS.API_ID}`"
-                    }
-                ]
-            },
-            {
-                "type": "section",
-                "fields": [
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Responsible:*\n{responsible}"
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Datetime:*\n{datetime.now()}"
-                    }
-                ]
-            },
-            {
-                "type": "divider"
-            },
-            {
-                "type": "section",
-                "text": {
+    blocks = [
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": repr(exception),
+                "emoji": True
+            }
+        },
+        {
+            "type": "section",
+            "fields": [
+                {
                     "type": "mrkdwn",
-                    "text": f"*Traceback:*\n```{stack_traces}```"
+                    "text": f"*Service:*\n{service_name}"
+                },
+                {
+                    "type": "mrkdwn",
+                    "text": f"*Branch:*\n{get_git_branch()}"
                 }
-            },
-        ]
-    }
-    response = requests.post(slack_url, json=payload)
+            ]
+        },
+        {
+            "type": "section",
+            "fields": [
+                {
+                    "type": "mrkdwn",
+                    "text": f"*Endpoint:*\n`{endpoint}`"
+                },
+                {
+                    "type": "mrkdwn",
+                    "text": f"*API ID:*\n`{GLOBALS.API_ID}`"
+                }
+            ]
+        },
+        {
+            "type": "section",
+            "fields": [
+                {
+                    "type": "mrkdwn",
+                    "text": f"*Responsible:*\n{responsible}"
+                },
+                {
+                    "type": "mrkdwn",
+                    "text": f"*Datetime:*\n{datetime.now()}"
+                }
+            ]
+        },
+        {
+            "type": "divider"
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"*Traceback:*\n```{stack_traces}```"
+            }
+        }
+    ]
+    response = requests.post(slack_url, json={
+        "text": repr(exception),
+        "blocks": blocks
+    })
     if not response.ok:
+        if response.text == 'invalid_blocks':
+            message = ":ghost: *Couldn't display errors traceback, message contains restricted" \
+                      " chars, please check manually on your database based on `API_ID`*"
+            blocks[-1].get('text').update({'text': message})
+            response = requests.post(slack_url, json={
+                "text": repr(exception),
+                "blocks": blocks
+            })
         print(response.text)
